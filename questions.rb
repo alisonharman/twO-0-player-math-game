70489e868670519a1796c333f6ab7af8b11943a2
Shaun Robinson <connecshaun@gmail.com>
2022-04-08 17:24:17 +0000

    Setup Question class with intial values and answer func

---
+++ questions.rb
@@ -0,0 +1,18 @@
+# will manage questions and correct answer and show the player the questions
+
+class Question
+  attr_accessor :number1, :number2, :question, :answer
+
+  #set initial values for the obj
+  def initialize()
+    @number1 = (rand(1..20)) #creates the random num between 1 and 20
+    @number2 = (rand(1..20))
+    @question = "What does #{number1} plus #{number2} equal?"
+    @answer = num1 + num2
+  end
+
+  def answer
+    @number1 + @number2
+  end
+end
+

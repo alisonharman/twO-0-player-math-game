4add931d059a653c6b8ba037e12619909ea47c96
Alison Harman <alison_harman@sympatico.ca>
2022-04-09 15:29:38 +0000

    add framework for game logic

--- main.rb
+++ main.rb
@@ -3,5 +3,21 @@ require './questions'
 require './game'
 require './players'
 
+# create players
+player1 = Player.new("Alison")
+player2 = Player.new("Shaun")
+
 #to start a new game
-game = Game.new
+game = Game.new(player1, player2)
+
+
+while game.game_on? do
+
+  #create new question and display
+  question = Question.new
+  puts question.question
+  #display answer
+  puts question.answer
+  
+  player1.incorrect #to avoid an infinite loop
+end

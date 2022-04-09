8dd800838f67492d7d4212b9f88f2de0c7114eb5
Alison Harman <alison_harman@sympatico.ca>
2022-04-09 15:30:18 +0000

    add initialize, reader, and game_on? functions

--- game.rb
+++ game.rb
@@ -2,4 +2,19 @@
 
 class Game
 
+  attr_reader :playerid
+
+  def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
+  end
+
+  def game_on?
+    if @player1.still_alive? && @player2.still_alive?
+      return true
+    else
+      return false
+    end
+  end
+
 end

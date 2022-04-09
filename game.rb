871bf6c205a91ec76545b52d8ce19a9002e3fedf
Alison Harman <alison_harman@sympatico.ca>
2022-04-09 18:23:29 +0000

    add logic for asking question, taking response, and switching players

--- game.rb
+++ game.rb
@@ -7,6 +7,34 @@ class Game
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
+    @current_player = player1
+  end
+
+  #def correct
+  #  player_response = question.answer
+  #end
+
+  def turn(question)
+    puts "-------- NEW TURN ---------"
+    puts "Player #{@current_player.playerid}: #{question.question}"
+    answ = gets.chomp.to_i
+    if answ == question.answer
+      puts "Player #{@current_player.playerid}: YES! You are correct."
+      puts "P1: #{@player1.score}/3 vs. P2: #{@player2.score}/3"
+    else
+      puts "Player #{@current_player.playerid}: Seriously? No!"
+      @current_player.incorrect
+      puts "P1: #{@player1.score}/3 vs. P2: #{@player2.score}/3"
+    end
+    switch_players
+  end
+
+  def switch_players
+    if @current_player == @player1
+      @current_player = @player2
+    else
+      @current_player = @player1
+    end
   end
 
   def game_on?
--- main.rb
+++ main.rb
@@ -4,20 +4,16 @@ require './game'
 require './players'
 
 # create players
-player1 = Player.new("Alison")
-player2 = Player.new("Shaun")
+player1 = Player.new("1")
+player2 = Player.new("2")
 
 #to start a new game
 game = Game.new(player1, player2)
-
 
 while game.game_on? do
 
   #create new question and display
   question = Question.new
-  puts question.question
-  #display answer
-  puts question.answer
   
-  player1.incorrect #to avoid an infinite loop
+  game.turn(question)
 end
